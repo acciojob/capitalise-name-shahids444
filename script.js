adee946ecdeeba0bfbe7702177a4fca9e81b18d0
shahids444 <shahidshaiks444@gmail.com>
2025-04-29 22:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //your JS code here. If required.
 function Uppercase(params) {
-	
+	let inputfield = document.getElementById("fname")
+	inputfield.value =  inputfield.value.toUpperCase();
 }
+document.getElementById("fname").addEventListener("blur",Uppercase)
